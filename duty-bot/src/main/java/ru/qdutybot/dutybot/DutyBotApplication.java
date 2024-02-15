c2030c700887a2dbb6d539940412ee75194d5e64
AlexReverse <mr.evteev.1998@gmail.com>
2024-02-15 22:53:24 +0300

    1

---
+++ duty-bot/src/main/java/ru/qdutybot/dutybot/DutyBotApplication.java
@@ -0,0 +1,13 @@
+package ru.qdutybot.dutybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyBotApplication.class, args);
+	}
+
+}
