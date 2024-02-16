3752ab422c6e6f5217cfee00a0d388f41860445a
AlexReverse <mr.evteev.1998@gmail.com>
2024-02-17 01:18:59 +0300

    Added startCommand, unknownCommand, sendMessage Extends TelegramLongPollingBot

---
+++ duty-bot/src/main/java/ru/qdutybot/dutybot/configuration/DutyBotConfiguration.java
@@ -0,0 +1,31 @@
+package ru.qdutybot.dutybot.configuration;
+
+
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.qdutybot.dutybot.bot.DutyBot;
+
+@Configuration
+public class DutyBotConfiguration {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(DutyBot dutyBot) {
+        try {
+            var api = new TelegramBotsApi(DefaultBotSession.class);
+            api.registerBot(dutyBot);
+            return api;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient();
+    }
+}
