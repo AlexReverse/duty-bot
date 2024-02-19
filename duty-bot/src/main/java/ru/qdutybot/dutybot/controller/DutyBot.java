ca16fad9fc7edbfe129ae83b287d36074d0d7b6c
AlexReverse <mr.evteev.1998@gmail.com>
2024-02-19 23:28:04 +0300

    Rework on setup Command and duty Command

--- duty-bot/src/main/java/ru/qdutybot/dutybot/controller/DutyBot.java
+++ duty-bot/src/main/java/ru/qdutybot/dutybot/controller/DutyBot.java
@@ -11,6 +11,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
@@ -25,6 +27,7 @@ public class DutyBot extends TelegramLongPollingBot {
 
     private static final Logger LOG = LoggerFactory.getLogger(DutyBot.class);
 
+
     public DutyBot(@Value("${bot.token}") String botToken) {
         super(botToken);
         List<BotCommand> commandList = new ArrayList<>();
@@ -32,41 +35,88 @@ public class DutyBot extends TelegramLongPollingBot {
         commandList.add(new BotCommand("/help", "просмотр информации"));
         commandList.add(new BotCommand("/duty", "уточнение дежурного"));
         commandList.add(new BotCommand("/setup", "назначить дежурного"));
-        try{
+        try {
             this.execute(new SetMyCommands(commandList, new BotCommandScopeDefault(), null));
         } catch (TelegramApiException e) {
             log.error("Error: ", e);
         }
     }
+
     @Override
     public void onUpdateReceived(Update update) {
-        if (!update.hasMessage() || !update.getMessage().hasText()) {
-            return;
-        }
-        var message = update.getMessage().getText();
-        var chatId = update.getMessage().getChatId();
-        switch (message) {
-            case "/start" -> startCommand(chatId, update.getMessage().getChat().getUserName());
-            case "/help" -> helpCommand(chatId);
-            case "/duty" -> dutyCommand(chatId);
-            case "/setup" -> setupCommand(chatId, message);
-            //#TODO
-            default -> unknownCommand(chatId);
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            if (message.contains("/setup")) {
+                setupCommand(chatId, message);
+            }
+
+            switch (message) {
+                case "/start" -> startCommand(chatId, update.getMessage().getChat().getUserName());
+                case "/help" -> helpCommand(chatId);
+                case "/duty" -> dutyCommand(chatId);
+                //#TODO
+                default -> unknownCommand(chatId);
+            }
+
+        } else if (update.hasCallbackQuery()) {
+            String callbackData = update.getCallbackQuery().getData();
+            Long messageId = Long.valueOf(update.getCallbackQuery().getMessage().getMessageId());
+            Long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callbackData.equals("TEAM1")) {
+                sendMessage(chatId, "team 1");
+            } else if (callbackData.equals("TEAM2")) {
+                sendMessage(chatId, "team 2");
+            } else if (callbackData.equals("TEAM3")) {
+                sendMessage(chatId, "team 3");
+            }
         }
     }
 
-    private void setupCommand(Long chatId, String string) {
+    private void setupCommand(Long chatId, String message) {
         var team = "test"; //#TODO
-        var text = "Дежурный успешно добавлен";
-        map.put(team, string);
+        message = message.substring(6);
+        String text = "Дежурный " + message +" успешно добавлен";
         sendMessage(chatId, text);
+        map.put(team, message);
     }
 
     private void dutyCommand(Long chatId) {
-        var text = "Текущий дежурный %s";
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Уточнение команды дежурного");
 
-        String string = map.lastEntry().getValue();
-        sendMessage(chatId, text+string);
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+
+        var button1 = new InlineKeyboardButton();
+        button1.setText("team1");
+        button1.setCallbackData("TEAM1");
+
+        var button2 = new InlineKeyboardButton();
+        button2.setText("team2");
+        button2.setCallbackData("TEAM2");
+
+        var button3 = new InlineKeyboardButton();
+        button3.setText("team3");
+        button3.setCallbackData("TEAM3");
+
+        rowInline.add(button1);
+        rowInline.add(button2);
+        rowInline.add(button3);
+
+        rowsInline.add(rowInline);
+
+        markup.setKeyboard(rowsInline);
+        message.setReplyMarkup(markup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            LOG.error("Ошибка отправки - ", e);
+        }
     }
 
     @Override
@@ -86,7 +136,9 @@ public class DutyBot extends TelegramLongPollingBot {
     private void helpCommand(Long chatId) {
         var text = """
                 /duty для уточнения дежурного
-                /setup для назнечения дежурного
+                
+                /setup {Имя дежурного} - для назнечения дежурного
+                Пример: /setup Александр
                 """;
         sendMessage(chatId, text);
     }
@@ -95,7 +147,7 @@ public class DutyBot extends TelegramLongPollingBot {
         sendMessage(chatId, "Не понимаю. У меня лапки!");
     }
 
-    private void sendMessage(Long chatId, String text){
+    private void sendMessage(Long chatId, String text) {
         var chatIdStr = String.valueOf(chatId);
         var sendMessage = new SendMessage(chatIdStr, text);
         try {
